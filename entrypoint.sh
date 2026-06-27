a70375c71d205d4b939f68935a37331292ae9631
omerkolsan <58569830+omerkolsan@users.noreply.github.com>
2026-06-27 19:43:36 +0300

    Add entrypoint script for FreeRADIUS container

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "========================================="
+echo " MikroTik FreeRADIUS Container Starting "
+echo "========================================="
+
+
+
+echo "Starting FreeRADIUS..."
+
+echo "......... OMER KOLSAN .........."
