58a36389362a35461033d9462995cebb5328f7c1
omerkolsan <58569830+omerkolsan@users.noreply.github.com>
2026-06-27 19:53:37 +0300

    Refactor build.sh to remove Tailscale support
    Removed Tailscale-related comments and updated versioning for freeradius.

--- build.sh
+++ build.sh
@@ -6,50 +6,22 @@
 # Updates tailscale respository and runs `docker build` with flags configured for 
 # docker distribution. 
 # 
-############################################################################
-#
-# WARNING: Tailscale is not yet officially supported in Docker,
-# Kubernetes, etc.
-#
-# It might work, but we don't regularly test it, and it's not as polished as
-# our currently supported platforms. This is provided for people who know
-# how Tailscale works and what they're doing.
-#
-# Our tracking bug for officially support container use cases is:
-#    https://github.com/tailscale/tailscale/issues/504
-#
-# Also, see the various bugs tagged "containers":
-#    https://github.com/tailscale/tailscale/labels/containers
-#
-############################################################################
 #
 # Set PLATFORM as required for your router model. See:
 # https://mikrotik.com/products/matrix
 #
 PLATFORM="linux/arm64"
-TAILSCALE_VERSION=1.96.3
-VERSION=0.1.40
+VERSION=1.0.0
 
 set -eu
 
-rm -f tailscale.tar
-
-if [ ! -d ./tailscale/.git ]
-then
-    git -c advice.detachedHead=false clone https://github.com/tailscale/tailscale.git --branch v$TAILSCALE_VERSION
-fi
-
-TS_USE_TOOLCHAIN="Y"
-cd tailscale && eval $(./build_dist.sh shellvars) && cd ..
+rm -f freeradius.tar
 
 docker buildx build \
-  --no-cache \
-  --build-arg TAILSCALE_VERSION=$TAILSCALE_VERSION \
-  --build-arg VERSION_LONG=$VERSION_LONG \
-  --build-arg VERSION_SHORT=$VERSION_SHORT \
-  --build-arg VERSION_GIT_HASH=$VERSION_GIT_HASH \
   --platform $PLATFORM \
   --builder arm64-builder \
-  --load -t ghcr.io/fluent-networks/tailscale-mikrotik:$VERSION .
+  --load -t mikrotik-freeradius:$VERSION .
 
-skopeo copy docker-daemon:ghcr.io/fluent-networks/tailscale-mikrotik:$VERSION docker-archive:tailscale.tar
+skopeo copy \
+    docker-daemon:mikrotik-freeradius:$VERSION \
+    docker-archive:freeradius.tar
